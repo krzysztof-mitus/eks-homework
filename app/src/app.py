ee408c8776b344610559d8613bf41b9a63cfafa9
Krzysztof Mitus <krzys79.m@gmail.com>
2024-06-23 01:05:59 +0200

    fix geolocation

--- app/src/app.py
+++ app/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import socket
 import os
+import geocoder
 
 app = Flask(__name__, static_folder=os.getenv('TEMPLATES_DIR', 'templates'))
 
@@ -14,8 +15,9 @@ def index():
 @app.route('/', methods=['GET'])
 def geolocation():
     remote_addr = request.remote_addr
-    response = requests.get(f'https://ipapi.co/{remote_addr}/json/')
-    geolocation = response.json()
+    #response = requests.get(f'https://ipapi.co/{remote_addr}/json/')
+    #geolocation = response.json()
+    geolocation = geocoder.ip(remote_addr)
     return render_template('geolocation.html',
                             env_name = os.getenv('ENV_NAME', 'DEFAULT_ENV'),
                             local_address = socket.gethostbyname(socket.gethostname()),
