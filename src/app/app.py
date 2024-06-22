6ef537adce05a4945770dcfca6904847b59d06ec
Krzysztof Mitus <krzys79.m@gmail.com>
2024-06-22 16:04:24 +0200

    add app and workflow

---
+++ src/app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import requests
+import socket
+import os
+
+app = Flask(__name__, static_folder=os.getenv('TEMPLATES_DIR', 'templates'))
+
+
+@app.route('/index.html', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['GET'])
+def echo():
+    remote_addr = request.remote_addr
+    response = requests.get(f'https://ipapi.co/{remote_addr}/json/')
+    geolocation = response.json()
+    return render_template('echo.html',
+                            env_name = os.getenv('ENV_NAME', 'DEFAULT_ENV'),
+                            local_address = socket.gethostbyname(socket.gethostname()),
+                            geo_location = geolocation)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 8080, debug = True)
---
+++ src/app/test_app.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index_route(self):
+        response = self.app.get('/index.html')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to index.html page', response.data)
+
+    def test_echo_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'environment name', response.data)
+        self.assertIn(b'local ip', response.data)
+        self.assertIn(b'geo-location', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
