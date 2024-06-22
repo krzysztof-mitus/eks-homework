2f73d382cf72d5eec11e4a981c54d475aa97ec1b
Krzysztof Mitus <krzys79.m@gmail.com>
2024-06-22 21:14:18 +0200

    add terraform helm_release

---
+++ terragrunt/terraform/iac/helm_app.tf
@@ -0,0 +1,20 @@
+data "aws_caller_identity" "current" {}
+data "aws_ecr_authorization_token" "token" {}
+
+resource "helm_release" "homework" {
+    name       = "homework-app"
+    chart      = "homework-app"
+    namespace  = "homework"    
+    repository = "oci://${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    repository_username = data.aws_ecr_authorization_token.token.user_name
+    repository_password = data.aws_ecr_authorization_token.token.password    
+
+    values = [
+        "${file("helm-values/homework-app/values.yaml")}"
+    ]
+    
+    set {
+        name = image.registry
+        value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    }
+}
